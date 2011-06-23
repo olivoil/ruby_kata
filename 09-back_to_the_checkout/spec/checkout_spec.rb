2d7d699b6291695ddece6ce3ef9993ca4854432a
oliv_oil <olivier.melcher@gmail.com>
2011-06-23 18:38:06 -0400

    _FILE_ instead of pwd

--- 09-back_to_the_checkout/spec/checkout_spec.rb
+++ 09-back_to_the_checkout/spec/checkout_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Checkout do
   hash_rule = {'A' => {1 => 50, 3 => 130}, 'B' => {1 => 30, 2 => 45}, 'C' => {1 => 20}, 'D' => {1 => 15}}   
-  parsers = { HashParser => hash_rule, YAMLParser => File.join(Dir.pwd, 'bin', 'rules.yml') }
+  parsers = { HashParser => hash_rule, YAMLParser => File.join(File.dirname(__FILE__), '..', 'bin', 'rules.yml') }
 
   parsers.each do |parser, rule|
     describe "with #{parser}" do
